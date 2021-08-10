bb72d1371cdbd3b3bff1924b9bfe5dc39724cf2a
Jethro Modequillo <mjolnir311@gmail.com>
2021-08-10 10:54:08 +0800

    Add searching in automated tests

--- tests/test_scraper.py
+++ tests/test_scraper.py
@@ -2,10 +2,16 @@ import unittest
 from scrapers import shops
 
 
-class TestQueries(unittest.TestCase):
+class TestAPI(unittest.TestCase):
     def testEncodeKwargs(self):
         test_result = shops.AbstractAPI.URLEncodeQuery(name="redmi note 10")
         self.assertEqual(
             test_result,
             {"name": "redmi%20note%2010"},
         )
+
+    def testShopeeSearch(self):
+        search_result = shops.search_shopee(keyword="redmi note 10")
+        self.assertIsNotNone(search_result)
+        for item in search_result:
+            self.assertIn("itemid", item)
