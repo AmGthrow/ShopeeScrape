93958e5ab7732a62ff26be2465276136d5eb4d07
Jethro Modequillo <jethro.modequillo@mensaphilippines.org>
2021-08-16 22:16:00 +0800

    Merge API and DB test into single module

--- tests.py
+++ tests.py
@@ -1,5 +1,6 @@
 import unittest
 from scrapers import shopee
+from data import database
 import utils
 import requests
 
@@ -25,3 +26,14 @@ class TestAPI(unittest.TestCase):
                                         item['shopid'])
             response = requests.get(link)
             self.assertEqual(response.status_code, 200)
+
+
+class TestDB(unittest.TestCase):
+    def setUp(self):
+        self.db = database.ShopeeDatabase()
+        self.db.create_tables()
+
+    def test_store_from_search(self):
+        # TODO: test this with an actual item's known data and verify db contents 
+        self.db.add_items(shopee.search(keyword="redmi note 10"))
+        print(self.db.query("SELECT * FROM ShopeeProducts").fetchone())
