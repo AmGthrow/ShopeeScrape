f4d4e958cb78ea673c43a9be00b2d96d01acf1c4
Jethro Modequillo <jethro.modequillo@mensaphilippines.org>
2021-08-18 11:59:04 +0800

    Improved ability to add query args in driver code

--- main.py
+++ main.py
@@ -1,14 +1,15 @@
 # TODO: Move this to another module? "main" is an ambiguous filename
 
 import json
+from utils import get_queries_from_config
 from scrapers import shopee
 from data.database import ShopeeDatabase
 
 def main():
     db = ShopeeDatabase("data/shopee.db")
-    to_search = json.load(open('config.json'))['search_queries']
+    to_search = get_queries_from_config()
     for search_query in to_search:
-        search_results = shopee.search(add_url=True, keyword=search_query)
+        search_results = shopee.search(add_url=True, **search_query)
         db.add_items(search_results)
 
 
