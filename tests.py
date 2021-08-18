182036e2a11f7de7a5f1dcbe73642234bad6c79a
Jethro Modequillo <jethro.modequillo@mensaphilippines.org>
2021-08-18 12:07:04 +0800

    Update test to conform with DB schema including URL

--- tests.py
+++ tests.py
@@ -35,5 +35,5 @@ class TestDB(unittest.TestCase):
 
     def test_store_from_search(self):
         # TODO: test this with an actual item's known data and verify db contents 
-        self.db.add_items(shopee.search(keyword="redmi note 10"))
+        self.db.add_items(shopee.search(add_url=True, keyword="redmi note 10"))
         print(self.db.query("SELECT * FROM ShopeeProducts").fetchone())
